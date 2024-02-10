57a53f1a434dd3ef8dfff75fb2f923db1682176b
brmi1103 <mitchell@brusseau.co>
2024-02-10 16:53:25 -0600

    updates for login/registration. #mab

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,33 +1,29 @@
+// client/src/services/authService.js
+
 import axios from 'axios';
 
 const API_URL = '/api/users/';
 
-// Register a new user
-const register = (userData) => {
+const register = async (userData) => {
   return axios.post(API_URL + 'register', userData);
 };
 
-// Log in a user
-const login = (email, password) => {
-  return axios.post(API_URL + 'login', { email, password })
-    .then((response) => {
-      if (response.data.token) {
-        // Save the token in local storage
-        localStorage.setItem('userToken', response.data.token);
-      }
-      return response.data;
-    });
+const login = async (email, password) => {
+  return axios.post(API_URL + 'login', { email, password }).then((response) => {
+    if (response.data.token) {
+      localStorage.setItem('userToken', response.data.token);
+    }
+    return response.data;
+  });
 };
 
-// Log out the current user
 const logout = () => {
   localStorage.removeItem('userToken');
 };
 
-// Check if user is authenticated
 const isAuthenticated = () => {
   const token = localStorage.getItem('userToken');
-  return !!token; // Convert token presence to boolean
+  return !!token;
 };
 
 export default {
