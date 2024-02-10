c4af131e2fb967c3c88fd9175bbb33158c062c28
brmi1103 <mitchell@brusseau.co>
2024-02-09 21:37:31 -0600

    Updated Navbar again

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-// Import your logo images if they are within the src directory
-import tempusIcon from '../assets/imgs/TempusIcon.png';
-import tempusName from '../assets/imgs/TempusName.png';
+import tempusIcon from '../assets/imgs/TempusIcon.png'; // Ensure this path is correct
 
 const Navbar = () => {
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
@@ -12,23 +10,26 @@ const Navbar = () => {
   return (
     <header className="header">
       <div className="container header-container">
-        <Link to="/" className="navbar-brand logo-container">
-          <img src={tempusIcon} alt="Logo Icon" className="logo" />
-          <img src={tempusName} alt="Logo Name" className="logo-name" />
-        </Link>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link to="/" className="navbar-brand logo-container">
+            <img src={tempusIcon} alt="Logo Icon" className="logo" />
+            {/* "Tempus" name that disappears on mobile */}
+            <span className="navbar-text logo-name d-none d-lg-inline">Tempus</span>
+          </Link>
           <button className="navbar-toggler" type="button" onClick={handleNavCollapse} aria-controls="navbarNavDropdown" aria-expanded={!isNavCollapsed} aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNavDropdown">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/features-demo">Features & Demo</Link>
+                <Link className="nav-link" to="/">Home</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/pricing">Pricing</Link>
               </li>
-              {/* React doesn't directly support Bootstrap's dropdown functionality out of the box. For dropdowns, consider using React Bootstrap or managing dropdown state with React. */}
+              <li className="nav-item">
+                <Link className="nav-link" to="/features-demo">Features & Demo</Link>
+              </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/about-contact-us">About & Contact Us</Link>
               </li>
